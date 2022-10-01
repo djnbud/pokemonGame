692a0465f484834475e899481c9d6ba06af7ef2c
David Norwood <djnbud@hotmail.co.uk>
2022-10-01 13:58:31 +0100

    Added stat calculator which means the damage calculator now looks correct. Basically the base stats are just used to calculator what they actually have. Note will need to add IV and EV to stat calculator at some point

--- inventory.js
+++ inventory.js
@@ -5,99 +5,134 @@ let localPokemon = new Map();
 let localItems = new Map();
 
 function initInventory() {
-    //normally here we will check what player has in inventory such as potions and pokeballs
-    //for now we will give player the same on init
-    playerPokeballs.set("pokeball", { amount: 10 });
-    localPokemon.set(0, {
-        id: "Emby",
-        nickname: "Emby",
-        details: { level: 1, attacks: [attacks.Tackle, attacks.Fireball], health: 45, maxHealth: 45, experience: 0 },
-    });
-    storedPokemon.set("localStorage", localPokemon);
-    storedPokemon.set("pcStorage", []);
+  //normally here we will check what player has in inventory such as potions and pokeballs
+  //for now we will give player the same on init
+  playerPokeballs.set("pokeball", { amount: 10 });
+  localPokemon.set(0, {
+    id: "Emby",
+    nickname: "Emby",
+    details: {
+      level: 5,
+      attacks: [attacks.Tackle, attacks.Fireball],
+      health: 45,
+      maxHealth: 45,
+      experience: 0,
+      attackStat: 49,
+      defenseStat: 49,
+      speedStat: 49,
+    },
+  });
+  let startPokemon = localPokemon.get(0);
+  let updatedPlayerPokemonStats = statCalculator(
+    startPokemon.details,
+    monsters[startPokemon.id]
+  );
 
-    localItems.set("potion", { amount: 5 });
+  startPokemon.details.maxHealth = updatedPlayerPokemonStats.newHealth;
+  startPokemon.details.health = updatedPlayerPokemonStats.newHealth;
+  startPokemon.details.attackStat = updatedPlayerPokemonStats.newAttack;
+  startPokemon.details.defenseStat = updatedPlayerPokemonStats.newDefense;
+  startPokemon.details.speedStat = updatedPlayerPokemonStats.newSpeed;
+
+  storedPokemon.set("localStorage", localPokemon);
+  storedPokemon.set("pcStorage", []);
+
+  setLocalPokemon(0, startPokemon);
+
+  localItems.set("potion", { amount: 5 });
 }
 
 function usePokeball(name) {
-    if (playerPokeballs.has(name)) {
-        let currentPokeBalls = playerPokeballs.get(name);
-        if (currentPokeBalls.amount > 0) {
-            currentPokeBalls.amount -= 1;
-            playerPokeballs.set(name, currentPokeBalls);
-            return true;
-        }
+  if (playerPokeballs.has(name)) {
+    let currentPokeBalls = playerPokeballs.get(name);
+    if (currentPokeBalls.amount > 0) {
+      currentPokeBalls.amount -= 1;
+      playerPokeballs.set(name, currentPokeBalls);
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 function getPokeballs() {
-    return playerPokeballs;
+  return playerPokeballs;
 }
 
 function getstoredPokemon() {
-    return storedPokemon;
+  return storedPokemon;
 }
 
 function getItems() {
-    return localItems;
+  return localItems;
 }
 
 function getLocalStoredPokemon() {
-    return storedPokemon.get("localStorage");
+  return storedPokemon.get("localStorage");
 }
 
 function getPcStoredPokemon() {
-    return storedPokemon.get("pcStorage");
+  return storedPokemon.get("pcStorage");
 }
 
 function setLocalPokemon(index, details) {
-    let localPokemon = storedPokemon.get("localStorage");
-    localPokemon.set(index, details);
+  let localPokemon = storedPokemon.get("localStorage");
+  localPokemon.set(index, details);
 }
 
 function setItem(id, details) {
-    localItems.set(id, details);
+  localItems.set(id, details);
 }
 
 function checkAllLocalPokemonHealth() {
-    let localPokemon = storedPokemon.get("localStorage");
-    for (let [key, value] of localPokemon) {
-        if (value.details.health > 0) {
-            return true;
-        }
+  let localPokemon = storedPokemon.get("localStorage");
+  for (let [key, value] of localPokemon) {
+    if (value.details.health > 0) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 //For if all pokemon are fainted during a battle so need to heal them back to full
 //Or when visiting poke centre
 function healAllLocalPokemon() {
-    let localPokemon = storedPokemon.get("localStorage");
-    localPokemon.forEach((value, key) => {
-        value.details.health = value.details.maxHealth;
-    });
+  let localPokemon = storedPokemon.get("localStorage");
+  localPokemon.forEach((value, key) => {
+    value.details.health = value.details.maxHealth;
+  });
 }
 
 function addPokemonToStorage(pokemon, nickname) {
-    //when retrieved a new pokemon check if room in storage
-    let localStorage = getLocalStoredPokemon();
-    let pcStorage = getPcStoredPokemon();
-    if (localStorage.size < 6) {
-        localStorage.set(localStorage.size, {
-            id: pokemon.name,
-            nickname: nickname,
-            details: {
-                level: pokemon.level,
-                attacks: pokemon.attacks,
-                health: pokemon.maxHealth,
-                maxHealth: pokemon.maxHealth,
-                experience: pokemon.experience,
-            },
-        });
-    } else {
-        pcStorage.push({
-            id: pokemon.name,
-            nickname: nickname,
-            details: { level: pokemon.level, attacks: pokemon.attacks },
-        });
-    }
+  //when retrieved a new pokemon check if room in storage
+  let localStorage = getLocalStoredPokemon();
+  let pcStorage = getPcStoredPokemon();
+  if (localStorage.size < 6) {
+    localStorage.set(localStorage.size, {
+      id: pokemon.name,
+      nickname: nickname,
+      details: {
+        level: pokemon.level,
+        attacks: pokemon.attacks,
+        health: pokemon.maxHealth,
+        maxHealth: pokemon.maxHealth,
+        experience: pokemon.experience,
+        attackStat: pokemon.attackStat,
+        defenseStat: pokemon.defenseStat,
+        speedStat: pokemon.speedStat,
+      },
+    });
+  } else {
+    pcStorage.push({
+      id: pokemon.name,
+      nickname: nickname,
+      details: {
+        level: pokemon.level,
+        attacks: pokemon.attacks,
+        health: pokemon.maxHealth,
+        maxHealth: pokemon.maxHealth,
+        experience: pokemon.experience,
+        attackStat: pokemon.attackStat,
+        defenseStat: pokemon.defenseStat,
+        speedStat: pokemon.speedStat,
+      },
+    });
+  }
 }
--- utils.js
+++ utils.js
@@ -1,91 +1,135 @@
 function rectangularCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
-        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
-        rectangle1.position.y + rectangle1.height >= rectangle2.position.y
-    );
+  return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+  );
 }
 
 function experienceCalculator(playerPokemon, enemyPokemon) {
-    let b = enemyPokemon.baseExpYield, //base Experience yield of enmey fainted pokemon
-        L = enemyPokemon.level, //level of fainted pokemon
-        Lp = playerPokemon.level, // levl of player pokemon
-        s = 1; // for potential exp share
-    //Main bracket
+  let b = enemyPokemon.baseExpYield, //base Experience yield of enmey fainted pokemon
+    L = enemyPokemon.level, //level of fainted pokemon
+    Lp = playerPokemon.level, // levl of player pokemon
+    s = 1; // for potential exp share
+  //Main bracket
 
-    let mainBrackCalc = Math.pow((2 * L + 10) / (L + Lp + 10), 2.5);
+  let mainBrackCalc = Math.pow((2 * L + 10) / (L + Lp + 10), 2.5);
 
-    return Math.floor(((b * L) / 5) * (1 / s) * mainBrackCalc + 1);
+  return Math.floor(((b * L) / 5) * (1 / s) * mainBrackCalc + 1);
 }
 
-function catchCalculator(ballCatchRate, pokemonCatchRate, maxHp, currentHp, pokemonStatus) {
-    let catchAttempt = Math.floor(Math.random() * 255);
+function statCalculator(pokemon, baseStats) {
+  let iv = 1; //individual value need to determine how to calculator
+  let ev = 1; //effort value need to determine how to calculator
+  let firstHPCalc = 2 * baseStats.maxHealth;
+  ev = ev / 4;
+  firstHPCalc += iv;
+  firstHPCalc += ev;
+  firstHPCalc = firstHPCalc * pokemon.level;
+  firstHPCalc = firstHPCalc / 100;
+  firstHPCalc += pokemon.level + 10;
 
-    let topValue = ((3 * maxHp - 2 * currentHp) * pokemonCatchRate * ballCatchRate) / (3 * maxHp);
+  //note ev and iv might be stat specific
+  let attackStat = otherStatCalculator(pokemon, baseStats.attackStat, ev, iv);
+  let defenseStat = otherStatCalculator(pokemon, baseStats.defenseStat, ev, iv);
+  let speedStat = otherStatCalculator(pokemon, baseStats.speedStat, ev, iv);
 
-    if (topValue < 1) {
-        topValue = 1;
-    }
-    if (pokemonStatus !== undefined) {
-        topValue += pokemonStatus;
-    }
+  return {
+    newHealth: Math.floor(firstHPCalc),
+    newAttack: Math.floor(attackStat),
+    newDefense: Math.floor(defenseStat),
+    newSpeed: Math.floor(speedStat),
+  };
+}
 
-    if (catchAttempt <= topValue) {
-        return true;
-    }
-    return false;
+function otherStatCalculator(pokemon, statValue, ev, iv) {
+  let firstCalc = 2 * statValue;
+  ev = ev / 4;
+  firstCalc += iv;
+  firstCalc += ev;
+  firstCalc = firstCalc * pokemon.level;
+  firstCalc = firstCalc / 100;
+  firstCalc += 5;
+
+  return firstCalc;
+}
+
+function catchCalculator(
+  ballCatchRate,
+  pokemonCatchRate,
+  maxHp,
+  currentHp,
+  pokemonStatus
+) {
+  let catchAttempt = Math.floor(Math.random() * 255);
+
+  let topValue =
+    ((3 * maxHp - 2 * currentHp) * pokemonCatchRate * ballCatchRate) /
+    (3 * maxHp);
+
+  if (topValue < 1) {
+    topValue = 1;
+  }
+  if (pokemonStatus !== undefined) {
+    topValue += pokemonStatus;
+  }
+
+  if (catchAttempt <= topValue) {
+    return true;
+  }
+  return false;
 }
 
 //Gen 5 calculation note that have not include certain calculations for now
 function damageCalculator(attackingPokemon, defendingPokemon, attackingMove) {
-    let level = attackingPokemon.level,
-        a = attackingPokemon.attackStat,
-        d = defendingPokemon.defenseStat,
-        power = attackingMove.power,
-        randomAmnt = Math.floor(Math.random() * (100 - 85 + 1) + 85),
-        weather = 1, //potentially add in weather
-        typeEfectiveness = typeChart[attackingMove.type][defendingPokemon.type],
-        critAttempt = Math.floor(Math.random() * 255),
-        crit = 1;
-    if (critAttempt < attackingPokemon.speedStat / 2) {
-        crit = 1.5;
-    }
+  let level = attackingPokemon.level,
+    a = attackingPokemon.attackStat,
+    d = defendingPokemon.defenseStat,
+    power = attackingMove.power,
+    randomAmnt = Math.floor(Math.random() * (100 - 85 + 1) + 85),
+    weather = 1, //potentially add in weather
+    typeEfectiveness = typeChart[attackingMove.type][defendingPokemon.type],
+    critAttempt = Math.floor(Math.random() * 255),
+    crit = 1;
+  if (critAttempt < attackingPokemon.speedStat / 2) {
+    crit = 1.5;
+  }
 
-    randomAmnt = randomAmnt / 100;
+  randomAmnt = randomAmnt / 100;
 
-    let part1Calc = (2 * level) / 5;
-    part1Calc += 2;
-    let attDef = a / d;
-    let part2Calc = part1Calc * power * attDef;
-    let overCalc = part2Calc / 50;
-    overCalc += 2;
-    overCalc = overCalc * weather;
-    overCalc = overCalc * crit;
-    overCalc = overCalc * randomAmnt;
-    overCalc = overCalc * typeEfectiveness;
+  let part1Calc = (2 * level) / 5;
+  part1Calc += 2;
+  let attDef = a / d;
+  let part2Calc = part1Calc * power * attDef;
+  let overCalc = part2Calc / 50;
+  overCalc += 2;
+  overCalc = overCalc * weather;
+  overCalc = overCalc * crit;
+  overCalc = overCalc * randomAmnt;
+  overCalc = overCalc * typeEfectiveness;
 
-    //after calculating the damage return and inflict
-    return Math.floor(overCalc);
+  //after calculating the damage return and inflict
+  return Math.floor(overCalc);
 }
 
 function createBlankSpace(id, appendTo) {
-    const button = document.createElement("div");
-    button.id = id;
-    button.width = "100%";
-    button.height = "100%";
-    button.animate = false;
+  const button = document.createElement("div");
+  button.id = id;
+  button.width = "100%";
+  button.height = "100%";
+  button.animate = false;
 
-    document.querySelector(appendTo).append(button);
+  document.querySelector(appendTo).append(button);
 }
 
 function createBackButton(id, appendTo, hide, show) {
-    const button = document.createElement("button");
-    button.innerHTML = "Back";
-    button.id = id;
-    document.querySelector("#" + appendTo).append(button);
-    document.querySelector("#" + id).addEventListener("click", (e) => {
-        document.querySelector("#" + hide).style.visibility = "hidden";
-        document.querySelector("#" + show).style.visibility = "visible";
-    });
+  const button = document.createElement("button");
+  button.innerHTML = "Back";
+  button.id = id;
+  document.querySelector("#" + appendTo).append(button);
+  document.querySelector("#" + id).addEventListener("click", (e) => {
+    document.querySelector("#" + hide).style.visibility = "hidden";
+    document.querySelector("#" + show).style.visibility = "visible";
+  });
 }
