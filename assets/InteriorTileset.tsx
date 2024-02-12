9bfb4a9a756bf78471ef6fc528c8a24d2d86a877
David Norwood <djnbud@hotmail.co.uk>
2024-02-12 23:47:32 +0000

    start towards interior map
    start towards interior map

---
+++ assets/InteriorTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="InteriorTileset" tilewidth="12" tileheight="12" tilecount="25" columns="5">
+ <image source="InteriorTileset.png" width="60" height="60"/>
+</tileset>
